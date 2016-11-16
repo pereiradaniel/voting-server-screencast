55dae641a8901313cc715b73a17cac3c26a25ee0
Daniel <truenorthtrainingcompany@gmail.com>
2016-11-16 11:16:32 -0500

    addMovie test passes

---
+++ test/immutable_spec.js
@@ -0,0 +1,47 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('immutability', () => {
+
+  describe('a number', () => {
+
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+      // Test implementation
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+
+    // More its can go here
+  });
+
+  describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+
+  });
+
+});
