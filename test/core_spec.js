74c36b1ee7588c6721797559b65d49273f0ba59f
Daniel <truenorthtrainingcompany@gmail.com>
2016-11-17 10:51:30 -0500

    Voting Vote on entry vote increments the tally for movie being voted on in key vote/pair, creates if doesn't already exist.

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,8 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+// Import application logic functions for testing
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -42,6 +43,53 @@ describe('application logic', () => {
       }));
     });
  
+  });
+
+  describe('vote', () => {
+
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      }));
+    });
+
   });
 
 });
